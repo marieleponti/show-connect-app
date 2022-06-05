a5b02e28784e387272b415d0ace042514f23667b
marieleponti <marieleponti@gmail.com>
2022-06-05 17:54:52 -0500

    router, added component for EventCard

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Icon</div>
+</template>
+
+<script></script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import BaseIcon from "@/components/BaseIcon.vue";
+
+Vue.component("BaseIcon", BaseIcon);
 
 Vue.config.productionTip = false;
 
