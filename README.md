9ae5a4170f283420fd91ee5b13e73ae2c334a94b
marieleponti <marieleponti@gmail.com>
2022-06-08 06:53:39 -0500

    readme.md

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# show-connect
-
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+App to plan events and connect to interested attendees. 
+Connects event hosts to venue managers. 
+In Vue and js. 
--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,12 +6,14 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users" />
       <span>{{ event.attendees.length }} attending</span>
     </div>
   </router-link>
 </template>
 
 <script>
+import BaseIcon from "./BaseIcon.vue";
 export default {
   data() {
     return {
@@ -28,6 +30,7 @@ export default {
       },
     };
   },
+  components: { BaseIcon },
 };
 </script>
 
