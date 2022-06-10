9d27f60198cf358b1133ffe3d2982c79dcda1956
marieleponti <marieleponti@gmail.com>
2022-06-10 09:58:10 -0500

    added global component slot

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -6,8 +6,11 @@
     <div class="event-card -shadow">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users" />
-      <span>{{ event.attendees.length }} attending</span>
+      <div>
+        <BaseIcon name="users">
+          {{ event.attendees.length }} attending
+        </BaseIcon>
+      </div>
     </div>
   </router-link>
 </template>
